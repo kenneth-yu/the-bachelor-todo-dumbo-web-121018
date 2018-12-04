74abcb9d6f14040963bc27fbadafe06062553290
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-12-04 16:43:58 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,10 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
   # code here
+  data.each do |season, info|
+    binding.pry
+  end
 end
 
 def get_contestant_name(data, occupation)
